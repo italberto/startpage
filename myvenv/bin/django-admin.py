da1b68574c6467f08ac3b95d2b67f45413bbd8f0
none <none@nome.com>
2017-08-25 15:16:56 -0300

    Início da aplicação.

---
+++ myvenv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/italberto/Dropbox/DevelopmentItalberto/startpage/myvenv/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
